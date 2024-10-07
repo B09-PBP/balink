f2f6d532e9c811d241260ce7d8dd81f15543f0da
Noc-art <nevinthang02@gmail.com>
2024-10-07 16:26:51 +0700

    add carousel

--- balink/urls.py
+++ balink/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
     path('auth/', include('authentication.urls')),
+    path('product/', include('product.urls'))
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_product_page(request):
+    context = {
+        'npm' : '2306123456',
+        'name': 'Pak Bepe',
+        'class': 'PBP E'
+    }
+
+    return render(request, "product_page.html", context)
+
+
