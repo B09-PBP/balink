481495dc0270422f3d260dd58b37f5921a021bb4
nabilahru <nabilahroslitau@gmail.com>
2024-10-24 14:50:24 +0700

    Memperbaiki add review by ajax

--- review/urls.py
+++ review/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from review.views import ride_to_review, create_review, add_review_entry_ajax
+from review.views import ride_to_review, add_review_entry_ajax
 from review.views import show_main, show_json, show_json_by_id
 
 app_name = 'review'
@@ -7,7 +7,6 @@ app_name = 'review'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('ride-to-review', ride_to_review, name='ride_to_review'),
-    path('create-review', create_review, name='create_review'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('add-review-entry-ajax', add_review_entry_ajax, name='add_review_entry_ajax')
--- review/views.py
+++ review/views.py
@@ -1,7 +1,8 @@
+from django.urls import reverse
 from product.models import Product
 from review.models import Review
 from review.forms import ReviewEntryForm
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
@@ -11,9 +12,9 @@ from django.utils.html import strip_tags
 
 # Create your views here.
 def show_main(request):
-    review = Review.objects.all()
+    reviews = Review.objects.all()
     context = {
-        'review' : review,
+        'reviews': reviews,
     }
     return render(request, "review.html", context)
 
@@ -22,44 +23,34 @@ def ride_to_review(request):
     form = ReviewEntryForm(request.POST or None)
 
     context = {
-        'products' : rides,
-        'form' : form,
+        'products': rides,
+        'form': form,
     }
 
     return render(request, "ride_to_review.html", context)
 
 @csrf_exempt
-def create_review(request):
-    if request.method == "POST":
-        user = request.user
-        ride = Product.objects.get(pk=request.POST.get("id"))
-        rating = request.POST.get("rating")
-        review_message = request.POST.get("review_message")
-
-        new_review = Review(user=user, ride=ride, rating=rating, review_message=review_message)
-        new_review.save()
-
-        return redirect('review')
-    return HttpResponseNotFound()
-
-@csrf_exempt
 @require_POST
 def add_review_entry_ajax(request):
-    user = request.user 
-    ride = get_object_or_404(Product, id=request.POST.get("id"))
+    user = request.user
+    ride = get_object_or_404(Product, id=request.POST.get("product_id"))
     rating = request.POST.get("rating")
     review_message = strip_tags(request.POST.get("review_message"))
 
-    new_review = Review(
-        user=user,
-        ride=ride,
-        rating=rating,
-        review_message=review_message,
-    )
-    
-    new_review.save() 
+    if rating and review_message:
+        new_review = Review(
+            user=user,
+            ride=ride,
+            rating=rating,
+            review_message=review_message,
+        )
+        new_review.save()
+        
+        return redirect(reverse('review:show_main'))
 
-    return HttpResponse(b"CREATED", status=201)
+    return render(request, 'review_form.html', {
+        'error_message': "Invalid input. Please fill all fields.",
+    })
 
 def show_json(request):
     data = Review.objects.all()
