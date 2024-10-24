882211a37a94246d552e0d5c9550fcf0de3b228f
Noc-art <nevinthang02@gmail.com>
2024-10-24 12:13:29 +0700

    product page, authentication fix

--- product/urls.py
+++ product/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-from product.views import show_product_page, search_products, show_xml, show_json, show_json_by_id, show_xml_by_id
+from product.views import show_product_page, show_product_detail, search_products, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 app_name = 'product'
 
 urlpatterns = [
     path('', show_product_page, name='show_product_page'),
+    path('detail/<uuid:id>', show_product_detail, name='show_product_detail'),
     path('search/', search_products, name='search_products'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),    
+    path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),    
 ]
--- product/views.py
+++ product/views.py
@@ -1,27 +1,23 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import HttpResponse
 from django.core import serializers
 
 def search_products(request):
     query = request.GET.get('query', '')
-    products = Product.objects.filter(name__icontains=query)[:5]  # Limit to 5 results
+    products = Product.objects.filter(name__icontains=query)[:5] 
     results = [{'name': product.name} for product in products]
     return JsonResponse(results, safe=False)
 
 def show_product_page(request):
-    # Get the search query from the request
     search_query = request.GET.get("search", "")
     
-    # Initialize products variable
-    products = Product.objects.all()  # Ensure products is initialized
+    products = Product.objects.all()  
 
-    # Filter products based on the search query
     if search_query:
         products = products.filter(name__icontains=search_query)
 
-    # Filter by price
     min_price = request.GET.get('min_price', None)
     max_price = request.GET.get('max_price', None)
     
@@ -31,7 +27,6 @@ def show_product_page(request):
     if max_price is not None and max_price != '':
         products = products.filter(price__lte=max_price)
 
-    # Filter by year
     min_year = request.GET.get('min_year', None)
     max_year = request.GET.get('max_year', None)
     
@@ -41,7 +36,6 @@ def show_product_page(request):
     if max_year is not None and max_year != '':
         products = products.filter(year__lte=max_year)
 
-    # Filter by km driven
     min_km = request.GET.get('min_km', None)
     max_km = request.GET.get('max_km', None)
     
@@ -51,11 +45,20 @@ def show_product_page(request):
     if max_km is not None and max_km != '':
         products = products.filter(km_driven__lte=max_km)
 
-    # Render the template with the products and search query
     return render(request, "product_page.html", {
         "products": products,
         "search_query": search_query,
     })
+
+def show_product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    related_products = Product.objects.filter(dealer=product.dealer).exclude(id=product.id)[:2]  # Get up to 6 related products
+
+    context = {
+        'product': product,
+        'related_products': related_products,
+    }
+    return render(request, 'product_detail.html', context)
 
 def show_xml(request):
     data = Product.objects.all()
