0572c593e3c6f117c5474b4e3054475e29fc6e11
Noc-art <nevinthang02@gmail.com>
2024-10-26 18:35:47 +0700

    main program clear

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Product
+from django.urls import reverse
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def setUp(self):
+        Product.objects.create(name="Test Product", price=99.99)
+
+    def test_product_creation(self):
+        product = Product.objects.get(name="Test Product")
+        self.assertEqual(product.price, 99.99)
+        self.assertEqual(str(product), "Test Product")
+
+
+
+class ProductViewTest(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name="Test Product", price=99.99)
+
+    def test_product_list_view(self):
+        response = self.client.get(reverse('product:show_product_page'))  
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Product")
+
