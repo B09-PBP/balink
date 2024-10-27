368b1a090ad642b1a20ae439c952e9809a4d502b
Noc-art <nevinthang02@gmail.com>
2024-10-27 20:49:25 +0700

    footer fix

--- landing_page/views.py
+++ landing_page/views.py
@@ -11,8 +11,8 @@ def about_us(request):
         {"name": "Nevin Thang", "npm": "2306203204", "image": "img/foto nevin.jpg"},
         {"name": "Shaine Glorvina Mathea", "npm": "2306245573", "image": "img/foto shaine.jpg"},
         {"name": "Nabilah Roslita Utami", "npm": "2306223446", "image": "img/foto bilah.jpg"},
-        {"name": "Name 4", "npm": "2100004", "image": "img/foto kinan.jpg"},
-        {"name": "Name 5", "npm": "2100005", "image": "img/foto mawla.jpg"},
+        {"name": "Shabrina Aulia Kinanti", "npm": "2306245472", "image": "img/foto kinan.jpg"},
+        {"name": "Mawla Raditya Pambudi", "npm": "2306275323", "image": "img/foto mawla.jpg"},
     ]
 
     context = {"team_members": team_members}
